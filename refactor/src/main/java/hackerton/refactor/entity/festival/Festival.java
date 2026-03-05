f2df80e84742ffe7319244d6b980dff15fcce008
Leedong-uk <du123kim@naver.com>
2026-03-05 11:50:31 +0900

    festival Entity 작성

---
+++ refactor/src/main/java/hackerton/refactor/entity/festival/Festival.java
@@ -0,0 +1,36 @@
+package hackerton.refactor.entity.festival;
+
+import hackerton.refactor.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Festival extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "festival_id")
+    private Long id;
+    private String address;
+    private String detailAddress;
+    private LocalDate eventStartDate;
+    private LocalDate eventEndDate;
+    private String firstImage;
+    private String tel;
+    private String festivalTitle;
+    @Lob
+    private String overview;
+
+    private Long contentId;
+    private String telName;
+    private double mapX;
+    private double mapY;
+}
