d7f8a02caf75ee97a6aabe0133e62a3c06981106
Leedong-uk <du123kim@naver.com>
2026-03-06 18:49:56 +0900

    예상하지 못한 예외 처리 handler 추가 (#7)

--- refactor/src/main/java/hackerton/refactor/general/advice/GlobalExceptionHandler.java
+++ refactor/src/main/java/hackerton/refactor/general/advice/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package hackerton.refactor.general.advice;
 import hackerton.refactor.general.exception.CustomException;
 import hackerton.refactor.general.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getBadStatusCode().getHttpStatus())
                 .body(ApiResponse.fail(e.getBadStatusCode().getHttpStatus().value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleAllException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(),"서버 내부 오류가 발생 하였습니다 "));
     }
 
 }
