8314e5845fe3cd0769d05355eed7a9d0d4f9ce14
Leedong-uk <du123kim@naver.com>
2026-03-14 15:08:20 +0900

    즐겨찾기 구현 완료

--- refactor/src/main/java/hackerton/refactor/web/controller/favorite/FavoriteController.java
+++ refactor/src/main/java/hackerton/refactor/web/controller/favorite/FavoriteController.java
@@ -1,11 +1,13 @@
 package hackerton.refactor.web.controller.favorite;
 
 import hackerton.refactor.domain.dto.document.DocumentCheckItemDto;
-import hackerton.refactor.domain.dto.document.DocumentInsertRequest;
+import hackerton.refactor.domain.dto.favorite.FavoriteResponse;
+import hackerton.refactor.domain.service.FavoriteService;
 import hackerton.refactor.domain.service.MemberDocumentCheckService;
 import hackerton.refactor.general.response.ApiSuccess;
 import hackerton.refactor.general.security.userdetail.CustomUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +18,33 @@ import java.util.List;
 public class FavoriteController {
 
     private final MemberDocumentCheckService memberDocumentCheckService;
+    private final FavoriteService favoriteService;
 
     @GetMapping("/test/{announceId}")
     @ApiSuccess()
     public List<DocumentCheckItemDto> test (@AuthenticationPrincipal CustomUser user, @PathVariable Long announceId) {
         return memberDocumentCheckService.getAllChecklist(user.getMemberId(), announceId);
+    }
+
+    //favorite 추가
+    @PostMapping("/favorite/{announceId}")
+    @ApiSuccess(message = "favorite.add")
+    public void addFavorite(@AuthenticationPrincipal CustomUser user, @PathVariable Long announceId) {
+        favoriteService.addFavorite(announceId,user.getMemberId());
+    }
+
+    //favorite 삭제
+    @DeleteMapping("/favorite/{announceId}")
+    @ApiSuccess(message="favorite.delete")
+    public void removeFavorite(@AuthenticationPrincipal CustomUser user, @PathVariable Long announceId) {
+        favoriteService.removeFavorite(announceId,user.getMemberId());
+    }
+
+    //favorite list 가져오기
+    @GetMapping("/favorite")
+    @ApiSuccess()
+    public List<FavoriteResponse> getFavorites(@AuthenticationPrincipal CustomUser user , Pageable pageable) {
+        return favoriteService.getFavorites(user.getMemberId(), pageable);
     }
 
 
